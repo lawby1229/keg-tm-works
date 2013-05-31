20574f46b219f9efc200ac467967dededbf8a2cb
Yuan Bozhi <lawby1229@163.com>
2013-05-31 14:57:52 +0800

    commit the result2

--- src/keg/edu/mp/svm/Future.java
+++ src/keg/edu/mp/svm/Future.java
@@ -137,53 +137,67 @@ public class Future {
 
 	public static void main(String law[]) {
 		// Future f = new Future("D:\\移动原始数据\\移动\\校园用户用户整体清单_no.csv", 3);
-		String[] arg = { ".\\heart_scale", // 存放SVM训练模型用的数据的路径
-				".\\mode" }; // 存放SVM通过训练数据训练出来的模型的路径
-		// String[] arg0 = { ".\\train_file0", // 存放SVM训练模型用的数据的路径
-		// ".\\mode0" }; // 存放SVM通过训练数据训练出来的模型的路径
+		// String[] arg = { ".\\heart_scale", // 存放SVM训练模型用的数据的路径
+		// ".\\mode" }; // 存放SVM通过训练数据训练出来的模型的路径
+		String[] arg0 = { ".\\train_file0", ".\\mode0_L", "-t", "0" };
 		// String[] arg1 = { ".\\train_file1", // 存放SVM训练模型用的数据的路径
 		// ".\\mode1" }; // 存放SVM通过训练数据训练出来的模型的路径
 		// String[] arg2 = { ".\\train_file2", // 存放SVM训练模型用的数据的路径
 		// ".\\mode2" }; // 存放SVM通过训练数据训练出来的模型的路径
-//---------------------------------------------------------------------------------------------------
-		String[] parg = { ".\\heart_scale", // 这个是存放测试数据
-				".\\mode", // 调用的是训练以后的模型
-				".\\predict" }; // 生成的结果的文件的路径
 
-		String[] parg0_1 = { ".\\train_file1", // 这个是存放测试数据
-				".\\mode0", // 调用的是训练以后的模型
-				".\\predict0_1" }; // 生成的结果的文件的路径
-		String[] parg0_2 = { ".\\train_file2", // 这个是存放测试数据
-				".\\mode0", // 调用的是训练以后的模型
-				".\\predict0_2" }; // 生成的结果的文件的路径
+		// String[] arg01 = { ".\\train_file0", ".\\mode0_l" };
+		// String[] arg12 = { ".\\train_file1", ".\\mode1_l" };
+		// String[] arg02 = { ".\\train_file2", ".\\mode2_l" };
+		// ---------------------------------------------------------------------------------------------------
+		// String[] parg = { ".\\heart_scale", // 这个是存放测试数据
+		// ".\\mode", // 调用的是训练以后的模型
+		// ".\\predict" }; // 生成的结果的文件的路径
 
-		String[] parg1_0 = { ".\\train_file0", // 这个是存放测试数据
-				".\\mode1", // 调用的是训练以后的模型
-				".\\predict1_0" }; // 生成的结果的文件的路径
-		String[] parg1_2 = { ".\\train_file2", // 这个是存放测试数据
-				".\\mode1", // 调用的是训练以后的模型
-				".\\predict1_2" }; // 生成的结果的文件的路径
+		String[] parg0_1 = { ".\\train_file0", // 这个是存放测试数据
+				".\\mode0_L", // 调用的是训练以后的模型
+				".\\predict0_0_L" }; // 生成的结果的文件的路径
+		// String[] parg0_2 = { ".\\train_file2", // 这个是存放测试数据
+		// ".\\mode0", // 调用的是训练以后的模型
+		// ".\\predict0_2" }; // 生成的结果的文件的路径
+		//
+		// String[] parg1_0 = { ".\\train_file0", // 这个是存放测试数据
+		// ".\\mode1", // 调用的是训练以后的模型
+		// ".\\predict1_0" }; // 生成的结果的文件的路径
+		// String[] parg1_2 = { ".\\train_file2", // 这个是存放测试数据
+		// ".\\mode1", // 调用的是训练以后的模型
+		// ".\\predict1_2" }; // 生成的结果的文件的路径
+		//
+		// String[] parg2_0 = { ".\\train_file0", // 这个是存放测试数据
+		// ".\\mode2", // 调用的是训练以后的模型
+		// ".\\predict2_0" }; // 生成的结果的文件的路径
+		// String[] parg2_1 = { ".\\train_file1", // 这个是存放测试数据
+		// ".\\mode2", // 调用的是训练以后的模型
+		// ".\\predict2_1" }; // 生成的结果的文件的路径
 
-		String[] parg2_0 = { ".\\train_file0", // 这个是存放测试数据
-				".\\mode2", // 调用的是训练以后的模型
-				".\\predict2_0" }; // 生成的结果的文件的路径
-		String[] parg2_1 = { ".\\train_file1", // 这个是存放测试数据
-				".\\mode2", // 调用的是训练以后的模型
-				".\\predict2_1" }; // 生成的结果的文件的路径
+		// String[] parg01_2 = { ".\\train_file2", ".\\mode01", ".\\predict01_2"
+		// };
+		// String[] parg12_0 = { ".\\train_file0", ".\\mode12", ".\\predict12_0"
+		// };
+		// String[] parg02_1 = { ".\\train_file1", ".\\mode02", ".\\predict02_1"
+		// };
 		System.out.println("........SVM运行开始..........");
 
 		try {
-			// svm_train.main(arg0);
+//			svm_train.main(arg0);
 			// svm_train.main(arg1);
 			// svm_train.main(arg2);
-			// svm_predict.main(parg0_1);
+			svm_predict.main(parg0_1);
 			// svm_predict.main(parg0_2);
 			// svm_predict.main(parg1_0);
 			// svm_predict.main(parg1_2);
 			// svm_predict.main(parg2_0);
 			// svm_predict.main(parg2_1);
-			svm_train.main(arg);
-			svm_predict.main(parg);
+			// svm_train.main(arg01);
+			// svm_train.main(arg12);
+			// svm_train.main(arg02);
+			// svm_predict.main(parg01_2);
+			// svm_predict.main(parg12_0);
+			// svm_predict.main(parg02_1);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
