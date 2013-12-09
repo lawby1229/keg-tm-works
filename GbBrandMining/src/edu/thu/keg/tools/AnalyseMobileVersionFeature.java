67cf4a94de416c2ebccfbbbf044ac3ff919ff0b4
Yuan Bozhi <lawby1229@163.com>
2013-12-09 09:13:22 +0800

    新文件

--- GbBrandMining/src/edu/thu/keg/tools/AnalyseMobileVersionFeature.java
+++ GbBrandMining/src/edu/thu/keg/tools/AnalyseMobileVersionFeature.java
@@ -14,11 +14,13 @@ import java.util.List;
 public class AnalyseMobileVersionFeature {
 
 	HashMap<String, Integer> Dimensions;
+	List<String> DimensionsArray;
 	List<HashMap<Integer, String>> FeaturesAllLine;
 	List<String> Labels;
 
 	AnalyseMobileVersionFeature() {
 		Dimensions = new HashMap<String, Integer>();
+		DimensionsArray = new ArrayList<>();
 		FeaturesAllLine = new ArrayList<>();
 		Labels = new ArrayList<>();
 	}
@@ -30,9 +32,11 @@ public class AnalyseMobileVersionFeature {
 			LineNumberReader lnr = new LineNumberReader(fr);
 			String line = lnr.readLine();
 			int lineNum = 0;
+			DimensionsArray.add("");
 			while (line != null) {
 				String[] features = line.split(",");
 				lineNum++;
+
 				if (features.length > 1) {
 					HashMap<Integer, String> lineFeature = new HashMap<>();
 					Labels.add(features[0]);
@@ -41,6 +45,7 @@ public class AnalyseMobileVersionFeature {
 						System.out.println(features.length + " " + feature[0]
 								+ ":" + feature[1]);
 						if (!Dimensions.containsKey(feature[0].trim())) {
+							DimensionsArray.add(feature[0].trim());
 							Dimensions.put(feature[0].trim(),
 									Dimensions.size() + 1);
 						}
@@ -65,22 +70,32 @@ public class AnalyseMobileVersionFeature {
 			FileWriter fw1 = new FileWriter(fileName + "_Dimensions");
 			FileWriter fw = new FileWriter(fileName);
 			Iterator itKey = Dimensions.keySet().iterator();
-			while (itKey.hasNext()) {
-				String key = (String) itKey.next();
-				fw1.write(Dimensions.get(key) + ":" + key + "\n");
+			// while (itKey.hasNext()) {
+			// String key = (String) itKey.next();
+			// fw1.write(Dimensions.get(key) + ":" + key + "\n");
+			// }
+			fw.write(",");
+			for (int j = 1; j < DimensionsArray.size(); j++) {
+
+				fw.write(DimensionsArray.get(j) + ",");
+
 			}
+			fw.write("\b\n");
 			fw1.flush();
 			for (int i = 0; i < FeaturesAllLine.size(); i++) {
-				fw.write(Labels.get(i) + " ");
+				fw.write(Labels.get(i) + ",");
 				HashMap<Integer, String> lineFeature = FeaturesAllLine.get(i);
 				itKey = lineFeature.keySet().iterator();
-				while (itKey.hasNext()) {
-					int key = (int) itKey.next();
-					fw.write(key + ":" + lineFeature.get(key) + ",");
+				for (int j = 1; j <= Dimensions.size(); j++) {
+					if (lineFeature.containsKey(j))
+						fw.write(lineFeature.get(j) + ",");
+					else
+						fw.write("" + ",");
 				}
 				fw.write("\b\n");
 				fw.flush();
 			}
+
 			fw.close();
 			fw1.close();
 		} catch (IOException e) {
