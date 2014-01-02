9c189967c189472d56c32d10e561f5bef24da2f9
Yuan Bozhi <lawby1229@163.com>
2014-01-03 01:18:13 +0800

    加入tfidf的维度更新

--- GbBrandMining/src/edu/thu/keg/GB/http0702/brand/features/MultiHostToFeatureExtra.java
+++ GbBrandMining/src/edu/thu/keg/GB/http0702/brand/features/MultiHostToFeatureExtra.java
@@ -19,9 +19,12 @@ import edu.thu.keg.GB.http0702.brand.iimmfilter.BrandChangeFilter;
  * 
  */
 public class MultiHostToFeatureExtra {
-	final static HashMap<String, Integer> FeatureMap = new HashMap<>();// 其中值是从1开始的
-	final static HashMap<String, Integer> VersionMap = new HashMap<>();// 其中值是从1开始的
+	final static HashMap<String, Integer> FeatureMap2Int = new HashMap<>();// 其中值是从1开始的
+	final static HashMap<Integer, String> IntMap2Feature = new HashMap<>();// 其中值是从1开始的
 
+	final static HashMap<String, Integer> VersionMap = new HashMap<>();// 其中值是从1开始的
+	HashMap<String, Integer> FeatureSumMap = new HashMap<>();// 键值代表维度和全文出现总数总数
+	List<Integer> lineSumList = new ArrayList<Integer>();// 每行是单词总数
 	int DimensionOfClass = 16;
 	String trainTable = "";
 	String testTable = "";
@@ -41,6 +44,11 @@ public class MultiHostToFeatureExtra {
 		this.isVersionAsTag = false;
 	}
 
+	/**
+	 * 根据isTrainFile 更新trainFeature或者testFesture 如果加載過version就得到的是version当标签的
+	 * 
+	 * @param isTrainFile
+	 */
 	public void getFile(boolean isTrainFile) {
 		List<HashMap<Integer, Integer>> Features;
 		int[] Dis;
@@ -85,9 +93,9 @@ public class MultiHostToFeatureExtra {
 					Features.add(row);
 					System.out.println(i);
 				}
-				if (!FeatureMap.containsKey(host))
+				if (!FeatureMap2Int.containsKey(host))
 					continue;
-				int iKey = (int) FeatureMap.get(host);
+				int iKey = (int) FeatureMap2Int.get(host);
 				int iValue = 1;
 				if (row.containsKey(iKey)) {
 					iValue = row.get(iKey).intValue() + 1;
@@ -107,17 +115,19 @@ public class MultiHostToFeatureExtra {
 		return rs;
 	}
 
+	/**
+	 * 根据isTrainFile 往文件里写trainFile或者testFile 并且写分部文件
+	 * 
+	 * @param isTrainFile
+	 */
 	public void writeFeatureToFile(boolean isTrainFile) {
 		String tableName = "";
 		Iterator<HashMap<Integer, Integer>> it;
-		int[] Dis = null;
 		if (isTrainFile) {
 			tableName = trainTable;
-			Dis = trainDis;
 			it = trainFeatures.iterator();
 		} else {
 			tableName = testTable;
-			Dis = testDis;
 			it = testFeatures.iterator();
 		}
 		FileWriter fw = null;
@@ -137,14 +147,6 @@ public class MultiHostToFeatureExtra {
 				fw.write(rowStr + "\n");
 				fw.flush();
 			}
-			fw.close();
-			// 写分布文件
-			fw = new FileWriter("Dis_" + tableName + "_MobileSet_Base_Host_"
-					+ tag + ".txt");
-			for (int i = 0; i < Dis.length; i++) {
-				fw.write(i + " " + Dis[i] + "\n");
-			}
-			fw.flush();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -156,6 +158,86 @@ public class MultiHostToFeatureExtra {
 				e.printStackTrace();
 			}
 		}
+		writeDisFile(isTrainFile);
+	}
+
+	/**
+	 * 根据isTrainFile 往文件里写trainFile或者testFile的TFIDF特征提取后的文件 并且写分部文件
+	 * 
+	 * @param isTrainFile
+	 */
+	public void writeTfIdfFeatureToFile(boolean isTrainFile) {
+		String tableName = "";
+		Iterator<HashMap<Integer, Double>> it;
+		if (isTrainFile) {
+			tableName = trainTable;
+			it = getTfIdfFeature(trainFeatures).iterator();
+		} else {
+			tableName = testTable;
+			it = getTfIdfFeature(testFeatures).iterator();
+		}
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(tableName + "_MobileSet_Base_Host_TFIDF_" + tag
+					+ ".txt");
+			while (it.hasNext()) {
+				String rowStr = "";
+				HashMap<Integer, Double> row = it.next();
+				Integer[] keys = row.keySet().toArray(new Integer[0]);
+				Arrays.sort(keys);
+				rowStr = row.get(0) + "";
+				for (int i = 0; i < keys.length; i++) {
+					if (keys[i] == 0)
+						continue;
+					rowStr = rowStr + " " + keys[i] + ":" + row.get(keys[i]);
+				}
+				System.out.println(rowStr);
+				fw.write(rowStr + "\n");
+				fw.flush();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		writeDisFile(isTrainFile);
+	}
+
+	/**
+	 * 写分部文件，每次写文件后会调用
+	 * 
+	 * @param isTrainFile
+	 */
+	private void writeDisFile(boolean isTrainFile) {
+		int[] Dis = null;
+		String tableName = "";
+		if (isTrainFile) {
+			tableName = trainTable;
+			Dis = trainDis;
+		} else {
+			tableName = testTable;
+			Dis = testDis;
+		}
+		FileWriter fw;
+		try {
+			fw = new FileWriter("Dis_" + tableName + "_MobileSet_Base_Host_"
+					+ tag + ".txt");
+			for (int i = 0; i < Dis.length; i++) {
+				fw.write(i + " " + Dis[i] + "\n");
+			}
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public void loadHostDimension(String field, String tableName) {
@@ -165,7 +247,9 @@ public class MultiHostToFeatureExtra {
 		try {
 			int i = 1;
 			while (rs.next()) {
-				FeatureMap.put(rs.getString(1), i++);
+				IntMap2Feature.put(i, rs.getString(1));
+				FeatureMap2Int.put(rs.getString(1), i);
+				i++;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -200,6 +284,53 @@ public class MultiHostToFeatureExtra {
 
 	}
 
+	/**
+	 * 讲feature转换成tfidf的feature
+	 * 
+	 * @param feature
+	 * @return
+	 */
+	private List<HashMap<Integer, Double>> getTfIdfFeature(
+			List<HashMap<Integer, Integer>> feature) {
+		for (int i = 0; i < feature.size(); i++) {
+			HashMap<Integer, Integer> row = feature.get(i);
+			Iterator<Integer> itHost = row.keySet().iterator();
+			int sumLine = 0;
+			while (itHost.hasNext()) {
+				int hostId = itHost.next();
+				sumLine += row.get(hostId);
+				String host = IntMap2Feature.get(hostId);
+				if (!FeatureSumMap.containsKey(host))
+					FeatureSumMap.put(host, 1);
+				FeatureSumMap.put(host, FeatureSumMap.get(host) + 1);
+			}
+			lineSumList.add(sumLine);
+		}
+		List<HashMap<Integer, Double>> result = new ArrayList<HashMap<Integer, Double>>();
+		for (int i = 0; i < feature.size(); i++) {
+			HashMap<Integer, Integer> row = feature.get(i);
+			Iterator<Integer> itHost = row.keySet().iterator();
+			HashMap<Integer, Double> rowNew = new HashMap<>();
+			while (itHost.hasNext()) {
+				double tf = 0, idf = 0;
+				int hostId = itHost.next();
+				if (hostId == 0) {
+					rowNew.put(0, row.get(0) + 0.0);
+					continue;
+				}
+				tf = row.get(hostId) / lineSumList.get(i);
+				if (FeatureSumMap.get(IntMap2Feature.get(hostId)) == 0)
+					idf = 1;
+				else
+					idf = Math.log(lineSumList.size()
+							/ FeatureSumMap.get(IntMap2Feature.get(hostId)));
+				rowNew.put(hostId, tf * idf);
+			}
+			result.add(rowNew);
+		}
+		return result;
+	}
+
 	public static void main(String arg[]) {
 		// 对多分类的7种手机数据进行分类和预测的特征抽取
 		// MultiHostToBrandFeatExtra app = new MultiHostToBrandFeatExtra(
@@ -213,12 +344,12 @@ public class MultiHostToFeatureExtra {
 
 		MultiHostToFeatureExtra app = new MultiHostToFeatureExtra(
 				"X4_TRAIN_ONE_G500_ADDFUNC", "X42_TEST_BASE_HOST", "version");
-		// app.loadHostDimension("host", "X4_TRAIN_ONE_G500_ADDFUNC");
+		app.loadHostDimension("host", "X4_TRAIN_ONE_G500_ADDFUNC");
 		app.loadVersionDimension("version", "X4_TRAIN_ONE_G500_ADDFUNC");
-		// app.getFile(false);
-		// app.writeFeatureToFile(false);
-		// app.getFile(true);
-		// app.writeFeatureToFile(true);
+		app.getFile(false);
+		app.writeTfIdfFeatureToFile(false);
+		app.getFile(true);
+		app.writeTfIdfFeatureToFile(true);
 		// app = new MultiHostToFeatureExtra("Z3_TRAIN_ONE_G500T1K_ADDFUNC",
 		// "Z32_TEST_BASE_HOST", "C4");
 		// app.loadHostDimension("host", "Z3_TRAIN_ONE_G500T1K_ADDFUNC");
